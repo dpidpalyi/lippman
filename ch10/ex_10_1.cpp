5a008489e164832249a9221201899f0fc501589f
dpidpalyi <dpidpalyi@mail.ru>
2023-10-17 22:06:45 +0500

    refactore 10.1

--- ch10/ex_10_1.cpp
+++ ch10/ex_10_1.cpp
@@ -1,30 +1,11 @@
 #include <iostream>
-<<<<<<< HEAD
-#include <algorithm>
-#include <vector>
-=======
 #include <vector>
 #include <algorithm>
->>>>>>> 1ee8d1f4c9b3efd43214db71249b379ffcee604b
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
-<<<<<<< HEAD
-
-int main() {
-	vector<int> vi;
-	int needed, n;
-	cout << "Enter a number to count: ";
-	cin >> needed;
-	cout << "Enter numbers where to count: ";
-	while (cin >> n)
-		vi.push_back(n);
-
-	cout << "Number: " << needed << " occurs: " <<std::count(vi.cbegin(), vi.cend(), needed) << endl;
-=======
-using std::count;
 
 int main() {
 	int needed, n;
@@ -41,7 +22,6 @@ int main() {
 			cout << "Number: " << needed << " didn't find." << endl;
 	} else
 		cout << "Wrong input" << endl;
->>>>>>> 1ee8d1f4c9b3efd43214db71249b379ffcee604b
 
 	return 0;
 }
