0e5254eb4d9ffea8166b63c2b4d18147833c05c6
dpidpalyi <dpidpalyi@mail.ru>
2023-11-07 22:48:09 +0500

    add to 12.22

---
+++ ch12/12.22/ex_12_22.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "StrBlob.h"
+#include "ConstStrBlobPtr.h"
+
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+using std::string;
+
+int main() {
+	cout << "Enter file to read: ";
+	string filename;
+	if (cin >> filename) {
+		std::ifstream in(filename);
+		if (!in) {
+			cerr << "Cannot open file: " << filename << endl;
+		  return -1;
+		}
+		StrBlob sb;
+		for (std::string line; std::getline(in, line); sb.push_back(line))
+			;
+		ConstStrBlobPtr p = sb.cbegin();
+		for (StrBlob::size_type pos = 0; pos != sb.size(); ++pos, p.inc())
+			cout << pos + 1 << ":\t" << p.deref() << endl;
+
+	} else
+		cerr << "Wrong input file!" << endl;
+}
