63b0b1272db48ec8c742f853bb296b1583f4308e
dpidpalyi <dpidpalyi@mail.ru>
2023-12-14 12:16:20 +0300

    add to 14.5

---
+++ ch14/14.2/ex_8_8.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+
+#include "Sales_data.h"
+
+int main(int argc, char *argv[]) {
+  if (argc == 3) {
+    std::ifstream ifs(argv[1]);
+    std::ofstream ofs(argv[2], std::ofstream::app);
+    Sales_data total(ifs);  // variable to hold the running sum
+    if (ifs) {              // read first transaction
+      Sales_data trans;     // variable to hold data for the next transaction
+      while (read(ifs, trans)) {           // read the remaining transactions
+        if (total.isbn() == trans.isbn())  // check the isbns
+          total.combine(trans);            // update the running total
+        else {
+          print(ofs, total) << '\n';  // print the results
+          total = trans;              // process the next book
+        }
+      }
+      print(ofs, total) << '\n';   // print the last transaction
+    } else                         // there was no input
+      std::cerr << "No data?!\n";  // notify the user
+  } else
+    std::cerr << "Wrong command arguments!\n";
+
+  return 0;
+}
