10e201d9b4d80d92ae35c27db4093cfc07c477b6
dpidpalyi <dpidpalyi@mail.ru>
2023-12-16 22:17:17 +0300

    add to 14.17

--- ch14/ex_14_5_8_12_17.cpp
+++ ch14/ex_14_5_8_12_17.cpp
@@ -3,11 +3,11 @@
 class Vehicle {
 	friend std::ostream &operator<<(std::ostream &os, const Vehicle &v);
 	friend std::istream &operator>>(std::istream &is, Vehicle &v);
+	friend bool operator==(const Vehicle &lhs, const Vehicle &rhs);
+	friend bool operator!=(const Vehicle &lhs, const Vehicle &rhs);
 	public:
 		Vehicle(): manfctr(""), model(""), year(0), distance(0) {}
-		Vehicle(const std::string &mfc, const std::string &mdl, unsigned y, unsigned d): manfctr(mfc), model(mdl), year(y), distance(d) {}
-		Vehicle(std::istream &is) {
-			is >> manfctr >> model >> year >> distance;
+		Vehicle(const std::string &mfc, const std::string &mdl, unsigned y, unsigned d): manfctr(mfc), model(mdl), year(y), distance(d) {} Vehicle(std::istream &is) { is >> manfctr >> model >> year >> distance;
 		}
 	private:
 		std::string manfctr;
@@ -33,12 +33,22 @@ std::istream &operator>>(std::istream &is, Vehicle &v) {
 	return is;
 }
 
+bool operator==(const Vehicle &lhs, const Vehicle &rhs) {
+	return lhs.manfctr == rhs.manfctr && lhs.model == rhs.model && lhs.year == rhs.year;
+}
+
+bool operator!=(const Vehicle &lhs, const Vehicle &rhs) {
+	return !(lhs == rhs);
+}
+
 int main() {
 	Vehicle v1("BMW", "320", 2023, 1000);
 	Vehicle v2;
 	std::cin >> v2;
 	std::cout << v1 << std::endl;
 	std::cout << v2 << std::endl;
+	std::cout << (v1 == v2) << std::endl;
+	std::cout << (v2 == v2) << std::endl;
 
 	return 0;
 }
